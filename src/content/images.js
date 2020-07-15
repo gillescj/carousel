183bbc6da0838458e3ed7b7e4c8fc1ceaa07af41
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-15 15:23:28 -0400

    Add random image content

---
+++ src/content/images.js
@@ -0,0 +1,9 @@
+const images = [
+    'https://images.unsplash.com/photo-1540270776932-e72e7c2d11cd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80',
+    'https://images.unsplash.com/photo-1542029722221-1c7dc4b06781?ixlib=rb-1.2.1&auto=format&fit=crop&w=1568&q=80',
+    'https://images.unsplash.com/photo-1584551882802-ca081b505b49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1450&q=80',
+    'https://images.unsplash.com/photo-1542318421-1d7619edc046?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    'https://images.unsplash.com/photo-1542332213-31f87348057f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+];
+
+export default images;
