9d4d89054cf98958934e2d3fb462f4dab698c227
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-15 15:23:53 -0400

    Pass image content to Carousel component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Carousel from './Carousel';
+import images from '../content/images';
 
 const App = () => {
-    return <Carousel />;
+    return <Carousel images={images} />;
 };
 
 export default App;
